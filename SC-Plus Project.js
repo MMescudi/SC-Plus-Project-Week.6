3a8ad1617f8dd394bedd1b663739866abb09c988
MMescudi <michelle.dachs@hotmail.de>
2023-04-14 11:35:45 +0200

    updated html css and js

--- SC-Plus Project.js
+++ SC-Plus Project.js
@@ -40,30 +40,32 @@ let month = months[now.getMonth()];
 h3.innerHTML = `${day}, ${month} ${date}, ${hours}:${minutes}, ${year}`;
 
 function displayTemperature(response) {
-  console.log(response.data);
-  let temperaturElement = document.querySelector("#temperature");
+  console.log(response);
+
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
+  cityElement.innerHTML = response.data.city;
+  let conditionElement = document.querySelector("#description");
+  conditionElement.innerHTML = response.data.condition.description;
   let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
   let windElement = document.querySelector("#wind");
-  let iconElement = document.querySelector("#icon");
-
-  celsiusTemperature = response.data.main.temp;
-
-  temperaturElement.innerHTML = Math.round(celsiusTemperature);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  celsiusTemperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+
+  let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
 }
 
 function search(city) {
-  apiKey = "e766b056eefc0cbfcf74d994e0e180b7";
-  apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  key = "b3538b22709dco0f1833fa618t8f41ba";
+  apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
@@ -71,14 +73,14 @@ function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
-  console.log(cityInputElement.value);
 }
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 function searchLocation(position) {
+  console.log(position);
   let apiKey = "a969311cfcbb4a83dfad2cf7478397f9";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${position.coords.longitude}&lat=${position.coords.latitude}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
@@ -86,9 +88,6 @@ function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
-
-let currentLocationBtn = document.querySelector("#locationBtn");
-currentLocationBtn.addEventListener("click", getCurrentLocation);
 
 function showFahrenheit(event) {
   event.preventDefault();
@@ -112,4 +111,17 @@ fahrenheitLink.addEventListener("click", showFahrenheit);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsius);
 
+let currentLocationBtn = document.querySelector("#locationBtn");
+currentLocationBtn.addEventListener("click", getCurrentLocation);
+
 search("New York");
+/*
+function showPosition(position) {
+  let h1 = document.querySelector("#city")
+  h1.innerHTML = ``
+  console.log(position.coords.latitude);
+}
+
+function getCurrentPosition (){
+  navigator.geolocation.getCurrentPosition(showPosition);
+}*/
