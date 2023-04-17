c5c5d2acd9c89bea01395ac76f9039ea87400e9a
MMescudi <michelle.dachs@hotmail.de>
2023-04-17 14:18:05 +0200

    js adjustment

--- SC-Plus Project.js
+++ SC-Plus Project.js
@@ -55,7 +55,7 @@ function displayForcast(response) {
   let forecastHTML = `<div class="row">`;
 
   forecast.forEach(function (forecastDay, index) {
-    if (index < 4) {
+    if (index < 5) {
       forecastHTML =
         forecastHTML +
         `
