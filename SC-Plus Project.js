f1edfcf7812cbce80b3d6501bc724b58367c8da4
MMescudi <michelle.dachs@hotmail.de>
2023-07-03 19:06:08 +0200

    Update SC-Plus Project.js
    Changed Time Code

--- SC-Plus Project.js
+++ SC-Plus Project.js
@@ -1,50 +1,10 @@
 //Time & Date settings
-let now = new Date();
-let h3 = document.querySelector("h3");
-
-let date = now.getDate();
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
+function realTime() {
+  let today = moment().format("ddd, MMMM DD. YYYY, HH:mm");
+  let todayElement = document.querySelector("#time");
+  todayElement.innerHTML = today;
 }
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-let year = now.getFullYear();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let months = [
-  "Jan",
-  "Feb",
-  "March",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-let month = months[now.getMonth()];
-h3.innerHTML = `${day}, ${month} ${date}, ${hours}:${minutes}, ${year}`;
-
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-  return days[day];
-}
+setInterval(realTime, 1000);
 
 function displayForcast(response) {
   let forecast = response.data.daily;
